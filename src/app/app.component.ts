c48c06712ae4cd5a89bcbcde933b341744bb9890
Josue Garcia <josgar_14@hotmail.com>
2019-10-04 22:12:26 -0400

    Dynamically Changing Validators example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DynamicallyChangingValidators';
+export class AppComponent implements OnInit {
+  myFormModel: FormGroup;
+
+  countryCtrl: FormControl;
+  phoneCtrl: FormControl;
+
+  constructor(fb: FormBuilder){
+    this.myFormModel = fb.group({
+      country: [''],
+      phone: ['']
+    });
+  }
+
+  ngOnInit() {
+    this.countryCtrl = this.myFormModel.get('country') as FormControl;
+    this.phoneCtrl = this.myFormModel.get('phone') as FormControl;
+
+    this.countryCtrl.valueChanges.subscribe(country => {
+      if ('USA' === country) {
+        this.phoneCtrl.setValidators([Validators.minLength(10)]);
+      }else {
+        this.phoneCtrl.setValidators([Validators.minLength(11)]);
+      }
+      this.phoneCtrl.updateValueAndValidity();
+    })
+  };
 }
